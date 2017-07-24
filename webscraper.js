d89fa171b1e9a24e1c9e5b78076fc4ddf670cda7
Swathi Iyer <swathii@stanford.edu>
2017-07-24 13:19:20 -0700

    functioning selenium webscraper, dataset recollected

--- webscraper.js
+++ webscraper.js
@@ -9,22 +9,26 @@ build();
 if (require.main === module) {
   //Get folders
   const dirs = p => readdirSync(p).filter(f => statSync(p+"/"+f).isDirectory());
-  const folders = dirs(join(dirname(__dirname), 'product_classification_test_data'));
+  const folders = dirs(join(dirname(__dirname), 'fathom-products' , 'product_classification_test_data'));
+  driver.manage().window().maximize();
+  driver.manage().timeouts().setScriptTimeout(100000); // 10 seconds
 
-  for(var i = 0; i < folders.length; i++){
-      var currUrl = join('file://', dirname(__dirname), 'product_classification_test_data', folders[i], 'source.webarchive');
-      var filepath = join(dirname(__dirname), 'product_classification_test_data', folders[i], 'nodes.txt');
+
+  folders.forEach(function(item){
+      var currUrl = join('file://', dirname(__dirname), 'fathom-products' , 'product_classification_test_data', item, 'source.webarchive');
 
       //Get the page
       driver.get(currUrl);
 
       //Get dimensions and css data
-      driver.executeScript(function() {
-        var doc_dimensions = {};
+      driver.executeAsyncScript(function(dir) {
+        var callback = arguments[ arguments.length - 1 ];
+        var src = document.documentElement.outerHTML;
+        var node_properties = {};
         var all = document.getElementsByTagName("*");
         for (var j = 0; j < all.length; j++) {
              var curr = all[j].getBoundingClientRect();
-             doc_dimensions[j.toString()] = {
+             node_properties[j.toString()] = {
                                               "top" : curr.top,
                                               "bottom" : curr.bottom,
                                               "left" : curr.left,
@@ -35,12 +39,42 @@ if (require.main === module) {
                                             };
         }
 
-        return doc_dimensions;
-      }).then(function(dimensions) {
+        setTimeout(function () {
+           callback({
+              html: src,
+              css: node_properties,
+              folder : dir
+           });
+        });
+
+      }, item).then(function(doc_info) {
+        console.log(doc_info.folder);
+        var src_filepath = join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', doc_info.folder, 'source.html');
+        var nodes_filepath = join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', doc_info.folder, 'nodes.txt');
+
+        //Write html to file
+        writeFile(src_filepath, String(doc_info.html) , 'utf-8', (err) => {
+        	if (err) {
+            throw err;
+        	   console.log("ERROR: ", err);
+          }
+          console.log(src_filepath, "DONE");
+        });
+
         //Write data to file
-        writeFile(filepath, JSON.stringify(dimensions) , 'utf-8');
+        writeFile(nodes_filepath, JSON.stringify(doc_info.css) , 'utf-8', (err) => {
+          if (err) {
+            throw err;
+            console.log("ERROR: ", err);
+          }
+          console.log(nodes_filepath, "DONE");
+        });
 
       });
-  }
+
+      setTimeout(function () {}, 1000);
+
+  });
+
   driver.close();
 }
