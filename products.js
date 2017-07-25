6e1969be99a027047479f9b74a57c30c89d1a730
Swathi Iyer <swathii@stanford.edu>
2017-07-25 15:29:57 -0700

    fixed issues on 7/25 pull req

--- products.js
+++ products.js
@@ -9,7 +9,7 @@
  *
  * Testing:
  *    50 test products in product_classification_test_data folder
- *    All test sources are product detail pages (one product on this page)
+ *    All test sources are product detail pages (one main product on this page)
  *    Later, go on to extract products from a product index page (multiple products per page)
  *
  */
@@ -19,7 +19,10 @@ const {dirname, join} = require('path');
 const leven = require('leven');
 const {dom, props, out, rule, ruleset, score, type} = require('fathom-web');
 const {staticDom} = require('fathom-web/utils');
-var currDict = {};
+const tuningRoutines = {'title' : tunedTitleFnodes,
+                        'price' : tunedPriceFnodes,
+                        'image' : tunedImageFnodes};
+
 
 // const {domSort, staticDom} = require('../../utils');
 // const {Annealer} = require('../../optimizers');
@@ -39,211 +42,205 @@ var currDict = {};
 //   return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
 // }
 
-// function tunedImageFnodes() {
-//     function imageSize(fnode) {
-//       return fnode.element.offsetWidth * fnode.element.offsetHeight;
-//     }
-//
-//     function imageHasSrc(fnode) {
-//       return fnode.element.hasAttribute('src') && fnode.element.getAttribute('src') !== '';
-//     }
-//
-//     function imageTitle(fnode) {
-//       var title = document.getElementsByTagName('title')[1];
-//       if (title === undefined) {
-//         return 1;
-//       }
-//
-//       if (fnode.element.getAttribute('title') &&
-//          (fnode.element.getAttribute('title').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('title'))) ||
-//           fnode.element.getAttribute('alt') &&
-//          (fnode.element.getAttribute('alt').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('alt')))) {
-//                return 10000;
-//       }
-//       return 1;
-//     }
-//
-//     function keywords(fnode) {
-//       if(fnode.element.hasAttribute('src') && fnode.element.src.match(/(thumb|logo|icon)/i)){
-//         return 0;
-//       } else if (fnode.element.hasAttribute('src') && fnode.element.src.match(/(hero|main|product|large)/i)){
-//         return 1000;
-//       }
-//       return 1;
-//     }
-//
-//     function imageType(fnode){
-//       if(fnode.element.hasAttribute('src') && fnode.element.src.match(/(jpg|jpeg)/i)){
-//         return 1000;
-//       } else if (fnode.element.hasAttribute('src') && fnode.element.src.match(/(png|webp)/i)){
-//         return 0;
-//       }
-//       return 1;
-//     }
-//
-//     function titleInSrc(fnode){
-//       var title = document.getElementsByTagName('title')[1];
-//       if (title === undefined) {
-//         return 1;
-//       }
-//
-//       var arr = title.innerHTML.replace('|', '').split(' ');
-//       var regexstring = '';
-//       for(var i = 0; i < arr.length; i++){
-//         regexstring += arr[i];
-//         if(i !== arr.length - 1){
-//           regexstring += "|";
-//         }
-//       }
-//
-//       var regex = new RegExp(regexstring, "gi");
-//
-//       if(fnode.element.hasAttribute('src') && fnode.element.src.match(regex)){
-//         return 1000 * fnode.element.src.match(regex).length;
-//       }
-//
-//       return 1;
-//     }
-//
-//     const rules = ruleset(
-//       //get all images
-//       rule(dom('img'), type('images')),
-//
-//       //better score for larger images
-//       rule(type('images'), score(imageSize)),
-//
-//       //jpegs used more often than pngs
-//       rule(type('images'), score(imageType)),
-//
-//       //make sure image has src
-//       rule(type('images'), score(imageHasSrc)),
-//
-//       //image title matches page title
-//       rule(type('images'), score(imageTitle)),
-//
-//       //image title contained in image src
-//       rule(type('images'), score(titleInSrc)),
-//
-//       //not to be confused with the thumbnail or logo
-//       rule(type('images'), score(keywords)),
-//
-//       //return image with max score
-//       rule(type('images').max(), out('product-image'))
-//
-//     );
-//
-//     function contentFnodes(doc) {
-//         return rules.against(doc).get('product-image');
-//     }
-//
-//     return contentFnodes;
-// }
+function tunedImageFnodes() {
+    // function imageSize(fnode) {
+    //   return fnode.element.offsetWidth * fnode.element.offsetHeight;
+    // }
+    //
+    // function imageHasSrc(fnode) {
+    //   return fnode.element.hasAttribute('src') && fnode.element.getAttribute('src') !== '';
+    // }
+    //
+    // function imageTitle(fnode) {
+    //   var title = document.getElementsByTagName('title')[1];
+    //   if (title === undefined) {
+    //     return 1;
+    //   }
+    //
+    //   if (fnode.element.getAttribute('title') &&
+    //      (fnode.element.getAttribute('title').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('title'))) ||
+    //       fnode.element.getAttribute('alt') &&
+    //      (fnode.element.getAttribute('alt').includes(title.innerHTML) || title.innerHTML.includes(fnode.element.getAttribute('alt')))) {
+    //            return 10000;
+    //   }
+    //   return 1;
+    // }
+    //
+    // function keywords(fnode) {
+    //   if(fnode.element.hasAttribute('src') && fnode.element.src.match(/(thumb|logo|icon)/i)){
+    //     return 0;
+    //   } else if (fnode.element.hasAttribute('src') && fnode.element.src.match(/(hero|main|product|large)/i)){
+    //     return 1000;
+    //   }
+    //   return 1;
+    // }
+    //
+    // function imageType(fnode){
+    //   if(fnode.element.hasAttribute('src') && fnode.element.src.match(/(jpg|jpeg)/i)){
+    //     return 1000;
+    //   } else if (fnode.element.hasAttribute('src') && fnode.element.src.match(/(png|webp)/i)){
+    //     return 0;
+    //   }
+    //   return 1;
+    // }
+    //
+    // function titleInSrc(fnode){
+    //   var title = document.getElementsByTagName('title')[1];
+    //   if (title === undefined) {
+    //     return 1;
+    //   }
+    //
+    //   var arr = title.innerHTML.replace('|', '').split(' ');
+    //   var regexstring = '';
+    //   for(var i = 0; i < arr.length; i++){
+    //     regexstring += arr[i];
+    //     if(i !== arr.length - 1){
+    //       regexstring += "|";
+    //     }
+    //   }
+    //
+    //   var regex = new RegExp(regexstring, "gi");
+    //
+    //   if(fnode.element.hasAttribute('src') && fnode.element.src.match(regex)){
+    //     return 1000 * fnode.element.src.match(regex).length;
+    //   }
+    //
+    //   return 1;
+    // }
+    //
+    // const rules = ruleset(
+    //   //get all images
+    //   rule(dom('img'), type('images')),
+    //
+    //   //better score for larger images
+    //   rule(type('images'), score(imageSize)),
+    //
+    //   //jpegs used more often than pngs
+    //   rule(type('images'), score(imageType)),
+    //
+    //   //make sure image has src
+    //   rule(type('images'), score(imageHasSrc)),
+    //
+    //   //image title matches page title
+    //   rule(type('images'), score(imageTitle)),
+    //
+    //   //image title contained in image src
+    //   rule(type('images'), score(titleInSrc)),
+    //
+    //   //not to be confused with the thumbnail or logo
+    //   rule(type('images'), score(keywords)),
+    //
+    //   //return image with max score
+    //   rule(type('images').max(), out('product-image'))
+    //
+    // );
+    //
+    // function tuningRoutine(doc) {
+    //     return rules.against(doc).get('product-image');
+    // }
+    //
+    // return tuningRoutine;
+}
 
-// function tunedTitleFnodes() {
-//     const rules = ruleset(
-//       //get all title tags in the inserted fixture
-//       rule(dom('div#fixture > title'), type('titleish')),
-//
-//       //return image with max score
-//       rule(type('titleish').max(), out('product-title'))
-//
-//     );
-//
-//     function contentFnodes(doc) {
-//         return rules.against(doc).get('product-title');
-//     }
-//
-//     return contentFnodes;
-// }
+function tunedTitleFnodes() {
+    // const rules = ruleset(
+    //   //get all title tags in the inserted fixture
+    //   rule(dom('div#fixture > title'), type('titleish')),
+    //
+    //   //return image with max score
+    //   rule(type('titleish').max(), out('product-title'))
+    //
+    // );
+    //
+    // function tuningRoutine(doc) {
+    //     return rules.against(doc).get('product-title');
+    // }
+    //
+    // return tuningRoutine;
+}
 
-// function tunedPriceFnodes() {
-//
-//     function hasDollarSign(fnode){
-//       if(fnode.element.childNodes[0] && fnode.element.childNodes[0].nodeValue && fnode.element.childNodes[0].nodeValue.includes('$')){
-//         return 2;
-//       }
-//       return 1;
-//     }
-//
-//     function tagHasGoodCss(fnode){
-//       if(fnode.element.id.match(/price/i) || fnode.element.classList.contains(/price/i)){
-//         return 2;
-//       }
-//       return 1;
-//     }
-//
-//     function notSavingsAmount(fnode){
-//       if(fnode.element.childNodes[0] && fnode.element.childNodes[0].nodeValue &&
-//         (fnode.element.childNodes[0].nodeValue.includes('-') || window.getComputedStyle(fnode.element).getPropertyValue('text-decoration') === 'line-through')){
-//         return 0;
-//       }
-//       return 1;
-//     }
-//
-//
-//     const rules = ruleset(
-//       //get all elements that could contain the price
-//       rule(dom('span, div, li'), type('priceish')),
-//
-//       //bonus if direct text (not children) contains a dollar sign
-//       rule(type('priceish'), score(hasDollarSign)),
-//
-//       //look for good css within tag
-//       rule(type('priceish'), score(tagHasGoodCss)),
-//
-//       //not to be confused with amount off (minus sign, crossed off)
-//       rule(type('priceish'), score(notSavingsAmount)),
-//
-//       //return image with max score
-//       rule(type('priceish').max(), out('product-price'))
-//
-//     );
-//
-//     function contentFnodes(doc) {
-//         return rules.against(doc).get('product-price');
-//     }
-//
-//     return contentFnodes;
-// }
+function tunedPriceFnodes() {
+
+    // function hasDollarSign(fnode){
+    //   if(fnode.element.childNodes[0] && fnode.element.childNodes[0].nodeValue && fnode.element.childNodes[0].nodeValue.includes('$')){
+    //     return 2;
+    //   }
+    //   return 1;
+    // }
+    //
+    // function tagHasGoodCss(fnode){
+    //   if(fnode.element.id.match(/price/i) || fnode.element.classList.contains(/price/i)){
+    //     return 2;
+    //   }
+    //   return 1;
+    // }
+    //
+    // function notSavingsAmount(fnode){
+    //   if(fnode.element.childNodes[0] && fnode.element.childNodes[0].nodeValue &&
+    //     (fnode.element.childNodes[0].nodeValue.includes('-') || window.getComputedStyle(fnode.element).getPropertyValue('text-decoration') === 'line-through')){
+    //     return 0;
+    //   }
+    //   return 1;
+    // }
+    //
+    //
+    // const rules = ruleset(
+    //   //get all elements that could contain the price
+    //   rule(dom('span, div, li'), type('priceish')),
+    //
+    //   //bonus if direct text (not children) contains a dollar sign
+    //   rule(type('priceish'), score(hasDollarSign)),
+    //
+    //   //look for good css within tag
+    //   rule(type('priceish'), score(tagHasGoodCss)),
+    //
+    //   //not to be confused with amount off (minus sign, crossed off)
+    //   rule(type('priceish'), score(notSavingsAmount)),
+    //
+    //   //return image with max score
+    //   rule(type('priceish').max(), out('product-price'))
+    //
+    // );
+    //
+    // function tuningRoutine(doc) {
+    //     return rules.against(doc).get('product-price');
+    // }
+    //
+    // return tuningRoutine;
+}
 
 /**
  * Maintain state as we compare a series of DOMs, reporting the percent
  * difference at the end.
  */
 class DiffStats {
-    constructor(contentFnodes, feature) {
-        this.lengthOfExpectedTexts = 0;
-        this.lengthOfDiffs = 0;
+    constructor(tuningRoutine, feature) {
+        this.numTests = 0;
+        this.deviation = 0;
         this.feature = feature;
-        if (feature === 'image') {
-          this.contentFnodes = contentFnodes || tunedImageFnodes();
-        } else if (feature === 'title'){
-          this.contentFnodes = contentFnodes || tunedTitleFnodes();
-        } else {
-          this.contentFnodes = contentFnodes || tunedPriceFnodes();
-        }
+        this.tuningRoutine = tuningRoutine || tuningRoutines[feature];
     }
 
-    compare(expectedDom, sourceDom) {
-        var expectedText;
-        var gotText;
+    compare(expectedDom, sourceDom, dict) {
+        let expectedText;
+        let gotText;
         if (this.feature === 'image') {
           //compare images by src
           expectedText = expectedDom.body.firstChild.src;
-          gotText = this.contentFnodes(sourceDom).map(fnode => fnode.element.src)[0];
+          gotText = this.tuningRoutine(sourceDom, dict).map(fnode => fnode.element.src)[0];
         } else if (this.feature === 'title') {
           //compare innerHTML text of titles
           expectedText = expectedDom.head.firstChild.innerHTML;
-          gotText = this.contentFnodes(sourceDom).map(fnode => fnode.element.innerHTML)[0];
+          gotText = this.tuningRoutine(sourceDom, dict).map(fnode => fnode.element.innerHTML)[0];
         } else if (this.feature === 'price') {
           //strip whitespace and dollar sign if there is one when comparing price
           expectedText = expectedDom.body.firstChild.textContent.replace('$', '').trim();
-          gotText = this.contentFnodes(sourceDom).map(fnode => fnode.element.textContent.replace('$', '').trim())[0];
+          gotText = this.tuningRoutine(sourceDom, dict).map(fnode => fnode.element.textContent.replace('$', '').trim())[0];
         }
 
-        this.lengthOfExpectedTexts++;
+        this.numTests++;
         if(expectedText !== gotText) {
-          this.lengthOfDiffs++;
+          this.deviation++;
         }
 
         // Uncomment for debugging:
@@ -253,56 +250,56 @@ class DiffStats {
     }
 
     score() {
-        return this.lengthOfDiffs / this.lengthOfExpectedTexts * 100;
+        return this.deviation / this.numTests * 100;
     }
 }
+
+/*
+ * Reads/parses the node#->css dictionary collected by selenium, and creates a new map from HTMLObj -> CSS
+ */
 
 function createDict(item, sourceDom){
-  var nodesDict = JSON.parse(readFileSync(join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', item, 'nodes.txt'), 'utf-8'));
-  var elems = sourceDom.getElementsByTagName('*');
-  console.log(sourceDom.getElementsByTagName('*').length, Object.keys(nodesDict).length);
+  let nodesMap = new Map();
+  const numberToCss = JSON.parse(readFileSync(join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', item, 'nodes.txt'), 'utf-8'));
+  const elems = sourceDom.getElementsByTagName('*');
+  console.log(sourceDom.getElementsByTagName('*').length, Object.keys(numberToCss).length);
 
   //Match each node from the sourceDom to its css, and store in the global dict
-  for(let i = 0; i < Object.keys(nodesDict).length; i++){
-    currDict[elems[i]] = nodesDict[i.toString()];
+  for(let i = 0; i < elems.length; i++){
+    nodesMap.set(elems[i], numberToCss[i]);
   }
+  console.log(nodesMap.size);
+  return nodesMap;
 }
 
-
+/*
+ * Calculate overall score for one feature
+ * Args: folders - list of all test folders; feature - title/image/price; coeffs - tuning coeffs if any
+ */
 function deviationScore(folders, feature, coeffs = []) {
-    var stats;
-    if(feature === 'image') {
-      stats = new DiffStats(tunedImageFnodes(...coeffs), feature);
-    } else if (feature === 'title') {
-      stats = new DiffStats(tunedTitleFnodes(...coeffs), feature);
-    } else {
-      stats = new DiffStats(tunedPriceFnodes(...coeffs), feature);
-    }
+    const stats = new DiffStats(tuningRoutines[feature], feature);
 
     //For each test file, create the object-> css map, and run the comparison function
-    folders.forEach(function(item){
-      if(['macys', 'swatch'].includes(item)){return;} //random jsdom script tag parsing errors, ignore for now
-      const domFromFile = fileName => staticDom(readFileSync(join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', item, fileName)));
-
-      createDict(item, domFromFile('source.html'));
-      var pair = [domFromFile('expected-' + feature + '.html'), domFromFile('source.html')];
-      stats.compare(...pair);
-      console.log(stats.score());
-      currDict = {};
+    folders.forEach(function(store){
+      if(['macys', 'swatch'].includes(store)){return;} // HACK: random jsdom script tag parsing errors, ignore for now
+      const domFromFile = fileName => staticDom(readFileSync(join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', store, fileName)));
+      const dict = createDict(store, domFromFile('source.html'));
+      const pair = [domFromFile('expected-' + feature + '.html'), domFromFile('source.html')];
+      // stats.compare(pair[0], pair[1], dict);
+      // console.log(stats.score());
     });
 
-    return stats.score();
+    // return stats.score();
 }
 
 if (require.main === module) {
-    const features = ['title', 'image', 'price'];
-    const dirs = p => readdirSync(p).filter(f => statSync(p+"/"+f).isDirectory());
-    const folders = dirs(join(dirname(__dirname), 'fathom-products' , 'product_classification_test_data'));
-    // const folders = ['amazon'];
+    const folders_in = p => readdirSync(p).filter(f => statSync(p + "/" + f).isDirectory());
+    // const folders = folders_in(join(dirname(__dirname), 'fathom-products' , 'product_classification_test_data'));
+    const folders = ['amazon'];
 
     //For each feature, calculate score
-    features.forEach(function(f){
-      // deviationScore(folders, f);
-      console.log('% difference from ideal:', deviationScore(folders, f));
+    Object.keys(tuningRoutines).forEach(function(f){
+      deviationScore(folders, f);
+      //console.log('% difference from ideal:', deviationScore(folders, f));
     });
 }
--- webscraper.js
+++ webscraper.js
@@ -5,76 +5,64 @@ var driver = new webdriver.Builder().
 withCapabilities(webdriver.Capabilities.safari()).
 build();
 
+function writeToFile(path, thing){
+  writeFile(path, thing, 'utf-8', (err) => {
+    if (err) {
+      console.log("ERROR: ", err);
+      throw err;
+    }
+    console.log(path, "DONE");
+  });
+}
+
+function collectCSS(dir) {
+  let callback = arguments[arguments.length - 1];
+  let node_properties = {};
+  const src = document.documentElement.outerHTML;
+  const all = document.getElementsByTagName("*");
+  for (let j = 0; j < all.length; j++) {
+       const curr = all[j].getBoundingClientRect();
+       node_properties[j] = {
+                              "top" : curr.top,
+                              "bottom" : curr.bottom,
+                              "left" : curr.left,
+                              "right" : curr.right,
+                              "display" : all[j].style.display,
+                              "visibility" : all[j].style.visibility,
+                              "strikethrough" : window.getComputedStyle(all[j]).getPropertyValue('text-decoration')
+                            };
+  }
+
+  setTimeout(function () {
+     callback({
+        html: src,
+        css: node_properties,
+        folder : dir
+     });
+  });
+
+}
 
 if (require.main === module) {
   //Get folders
   const dirs = p => readdirSync(p).filter(f => statSync(p+"/"+f).isDirectory());
   const folders = dirs(join(dirname(__dirname), 'fathom-products' , 'product_classification_test_data'));
-  driver.manage().window().maximize();
-  driver.manage().timeouts().setScriptTimeout(100000); 
-
+  driver.manage().window().setSize(1680, 960);
+  driver.manage().timeouts().setScriptTimeout(100000);
 
   folders.forEach(function(item){
-      var currUrl = join('file://', dirname(__dirname), 'fathom-products' , 'product_classification_test_data', item, 'source.webarchive');
+      let currUrl = join('file://', dirname(__dirname), 'fathom-products' , 'product_classification_test_data', item, 'source.webarchive');
 
       //Get the page
       driver.get(currUrl);
 
       //Get dimensions and css data
-      driver.executeAsyncScript(function(dir) {
-        var callback = arguments[ arguments.length - 1 ];
-        var src = document.documentElement.outerHTML;
-        var node_properties = {};
-        var all = document.getElementsByTagName("*");
-        for (var j = 0; j < all.length; j++) {
-             var curr = all[j].getBoundingClientRect();
-             node_properties[j.toString()] = {
-                                              "top" : curr.top,
-                                              "bottom" : curr.bottom,
-                                              "left" : curr.left,
-                                              "right" : curr.right,
-                                              "display" : all[j].style.display,
-                                              "visibility" : all[j].style.visibility,
-                                              "strikethrough" : window.getComputedStyle(all[j]).getPropertyValue('text-decoration')
-                                            };
-        }
-
-        setTimeout(function () {
-           callback({
-              html: src,
-              css: node_properties,
-              folder : dir
-           });
-        });
-
-      }, item).then(function(doc_info) {
+      driver.executeAsyncScript(collectCSS, item).then(function(doc_info) {
         console.log(doc_info.folder);
-        var src_filepath = join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', doc_info.folder, 'source.html');
-        var nodes_filepath = join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', doc_info.folder, 'nodes.txt');
-
-        //Write html to file
-        writeFile(src_filepath, String(doc_info.html) , 'utf-8', (err) => {
-        	if (err) {
-            throw err;
-        	   console.log("ERROR: ", err);
-          }
-          console.log(src_filepath, "DONE");
-        });
-
-        //Write data to file
-        writeFile(nodes_filepath, JSON.stringify(doc_info.css) , 'utf-8', (err) => {
-          if (err) {
-            throw err;
-            console.log("ERROR: ", err);
-          }
-          console.log(nodes_filepath, "DONE");
-        });
-
-      });
-
-      setTimeout(function () {}, 1000);
-
+        const base = join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', doc_info.folder);
+        writeToFile(base + '/source.html', String(doc_info.html));
+        writeToFile(base + '/nodes.txt', JSON.stringify(doc_info.css));
+    });
   });
-
   driver.close();
 }
