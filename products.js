bd62aa1f41276f6ef862e3617c7087470ce31a1d
Swathi Iyer <swathii@stanford.edu>
2017-08-03 13:42:02 -0700

    split training and testing data; images accuracy at 90% on testing data

--- products.js
+++ products.js
@@ -16,21 +16,27 @@
 
 const {readFileSync, writeFile, readdirSync, statSync} = require('fs');
 const {dirname, join} = require('path');
-const leven = require('leven');
 const {dom, props, out, rule, ruleset, score, type} = require('fathom-web');
 const {Annealer} = require('fathom-web/optimizers');
 const {staticDom} = require('fathom-web/utils');
 const tuningRoutines = {
-                        // 'title' : {'routine': tunedTitleFnodes, 'coeffs': []},
-                        // 'price' : {'routine': tunedPriceFnodes, 'coeffs': []},
-                        'image' : {'routine': tunedImageFnodes, 'coeffs': [2.5, 2.5, 3, 0.5]}
+                        'title' : {'routine': tunedTitleFnodes, 'coeffs': []},
+                        // 'price' : {'routine': tunedPriceFnodes, 'coeffs': [1, 1, 0.5]},
+                        'image' : {'routine': tunedImageFnodes, 'coeffs': [1.9, 3.0, 4.2, 0.5, 0.1, 0.8, 1.3, 0.7, 0.4, 1, 0.1, 0.1, 1.3]}
                         };
+const VIEWPORT_HEIGHT = 960;
+const VIEWPORT_WIDTH = 1680;
 
-function tunedImageFnodes(nodeToCssMap, coeffImgSize = 2.5, coeffImgHasSrc = 2.5, coeffImgTitle = 3, coeffKeywords = 0.5) {
+function tunedImageFnodes(nodeToCssMap, coeffImgSize = 2.5, coeffImgHasSrc = 2.5, coeffImgTitle = 3,
+  coeffItemprop = 0.5, coeffBadKeywords = 1, coeffGoodKeywords = 0.5, coeffClassKeywords = 1,
+  coefftitlewords = 1, coeffAboveTheFold = 1, coeffLeftOfPage = 1, coeffSVGs = 1, coeffDataURLs = 1, titlewordsBase = 1.5) {
     let title = '';
 
     function imageSize(fnode) {
       const css = nodeToCssMap.get(fnode.element);
+      if((css.left - css.right) * (css.top - css.bottom) === 0){
+        return 1;
+      }
       return (css.left - css.right) * (css.top - css.bottom) * coeffImgSize;
     }
 
@@ -39,30 +45,92 @@ function tunedImageFnodes(nodeToCssMap, coeffImgSize = 2.5, coeffImgHasSrc = 2.5
     }
 
     function imageTitle(fnode) {
-      if (title === undefined) {
-        return 1;
-      }
-
-      if (fnode.element.getAttribute('title') &&
-         (fnode.element.getAttribute('title').includes(title) || title.includes(fnode.element.getAttribute('title'))) ||
-          fnode.element.getAttribute('alt') &&
-         (fnode.element.getAttribute('alt').includes(title) || title.includes(fnode.element.getAttribute('alt')))) {
-               return 100 * coeffImgTitle;
+      const attrs = ['title', 'alt'];
+      for(let i = 0; i < attrs.length; i++){
+        if(fnode.element.getAttribute(attrs[i]) &&
+            (fnode.element.getAttribute(attrs[i]).includes(title) || title.includes(fnode.element.getAttribute(attrs[i])))){
+              return 100 * coeffImgTitle;
+            }
       }
       return 1;
     }
+
+    function itemprop(fnode){
+      if(fnode.element.hasAttribute('itemprop') && fnode.element.getAttribute('itemprop').match(/(image|main|product|hero|feature)/i)){
+        return 1000 * coeffItemprop;
+      }
+    }
+
 
     function keywords(fnode) {
       if(fnode.element.hasAttribute('src') && fnode.element.src.match(/(thumb|logo|icon)/i)){
-        return 0.5;
-      } else if (fnode.element.hasAttribute('src') && fnode.element.src.match(/(hero|main|product|feature)/i) ||
-                 fnode.element.id.match(/(hero|main|product|large|feature)/i) ||
-                 fnode.element.classList[0] && fnode.element.classList[0].match(/(hero|main|product|primary|feature)/i) ||
-                 fnode.element.hasAttribute('itemprop') && fnode.element.getAttribute('itemprop').match(/(image|main|product|hero|feature)/i)){
-        return 1000 * coeffKeywords;
+        return 0.5 * coeffBadKeywords;
+      }
+
+      const attrs = ['src', 'id', 'alt', 'title'];
+      for(let i = 0; i < attrs.length; i++){
+        if(fnode.element.hasAttribute(attrs[i]) && fnode.element.getAttribute(attrs[i]).match(/(main|product|hero|feature|primary)/i)){
+          return 1000 * coeffGoodKeywords;
+        }
+      }
+
+      for(let i = 0; i < fnode.element.classList.length; i++){
+        if(fnode.element.classList[i].match(/(hero|main|product|primary|feature)/i)){
+          return 1000 * coeffClassKeywords;
+        }
       }
       return 1;
     }
+
+    function titlewords(fnode){
+      const titleWords = title.replace('|', '').split(' ');
+      const attrs = ['src', 'title', 'alt'];
+      let start = 1;
+      for(let j = 0; j < attrs.length; j++){
+        for(let i = 0; i < titleWords.length; i++){
+            if(titleWords[i].match(/^[a-z0-9]+$/i)){
+              const regex = new RegExp(titleWords[i], "i");
+              if(fnode.element.hasAttribute(attrs[j]) && fnode.element.getAttribute(attrs[j]).match(regex)){
+                start+= fnode.element.getAttribute(attrs[j]).match(regex).length;
+              }
+          }
+        }
+      }
+      //exaggerating the differences a little
+      return Math.pow(titlewordsBase, start) * coefftitlewords;
+    }
+
+    function aboveTheFold(fnode){
+      const css = nodeToCssMap.get(fnode.element);
+      if(css.top < VIEWPORT_HEIGHT){
+        return 1;
+      }
+      return 0.5 * coeffAboveTheFold;
+    }
+
+    function leftOfPage(fnode){
+      const css = nodeToCssMap.get(fnode.element);
+      if(css.left < VIEWPORT_WIDTH/2){
+        return 1;
+      }
+      return 0.5 * coeffLeftOfPage;
+    }
+
+    function notSVGs(fnode){
+      if(fnode.element.hasAttribute('src') && fnode.element.getAttribute('src').includes('.svg')){
+        return 0;
+      }
+      return 1 * coeffSVGs;
+    }
+
+    function dataURLs(fnode){
+      const css = nodeToCssMap.get(fnode.element);
+      if (fnode.element.hasAttribute('src') && fnode.element.getAttribute('src').includes('data:')){
+        return 0;
+      }
+      return 1 * coeffDataURLs;
+    }
+
 
     const rules = ruleset(
       //get all images
@@ -79,6 +147,24 @@ function tunedImageFnodes(nodeToCssMap, coeffImgSize = 2.5, coeffImgHasSrc = 2.5
 
       //punish/bonus for good/bad css in class, id, url, etc.
       rule(type('images'), score(keywords)),
+
+      //check if itemprop attribute has certain keywords
+      rule(type('images'), score(itemprop)),
+
+      //words in the title are in the src/image title/alt
+      rule(type('images'), score(titlewords)),
+
+      //static bonus for images visible without scrolling
+      rule(type('images'), score(aboveTheFold)),
+
+      //static bonus for images to the left of the page
+      rule(type('images'), score(leftOfPage)),
+
+      //eliminate images of type svg
+      rule(type('images'), score(notSVGs)),
+
+      //image src likely will not be a data-url
+      rule(type('images'), score(dataURLs)),
 
       //return image with max score
       rule(type('images').max(), out('product-image'))
@@ -111,11 +197,11 @@ function tunedTitleFnodes(nodeToCssMap) {
     return tuningRoutine;
 }
 
-function tunedPriceFnodes(nodeToCssMap) {
+function tunedPriceFnodes(nodeToCssMap, coeffDollarSign = 1, coeffCSS = 1, coeffSavings = 0.5) {
 
     function hasDollarSign(fnode){
       if(fnode.element.childNodes[0] && fnode.element.childNodes[0].nodeValue && fnode.element.childNodes[0].nodeValue.includes('$')){
-        return 2;
+        return 2 * coeffDollarSign;
       }
       return 1;
     }
@@ -123,7 +209,7 @@ function tunedPriceFnodes(nodeToCssMap) {
     function tagHasGoodCss(fnode){
       if(fnode.element.id.match(/(price|sale|deal)/i) || fnode.element.classList.contains(/(price|sale|deal)/i) || fnode.element.itemprop && fnode.element.itemprop.match(/price/i) ||
           fnode.element.classList[0] && fnode.element.classList[0].match(/(price|sale|deal)/i)){
-        return 2;
+        return 2 * coeffCSS;
       }
       return 1;
     }
@@ -131,7 +217,7 @@ function tunedPriceFnodes(nodeToCssMap) {
     function notSavingsAmount(fnode){
       const css = nodeToCssMap.get(fnode.element);
       if(css.strikethrough === 'line-through'){
-        return 0;
+        return coeffSavings;
       }
       return 1;
     }
@@ -177,13 +263,20 @@ class DiffStats {
         let expectedText;
         let gotText;
         if (this.feature === 'image') {
-          //compare images by src
+          //compare images by src, strip query params
 
-          // console.log(expectedDom.body.firstChild.outerHTML);
+          //console.log(expectedDom.body.firstChild.outerHTML);
           // expectedText = expectedDom.body.firstChild.outerHTML;
           // gotText = this.tuningRoutine(nodeToCssMap)(sourceDom).map(fnode => fnode.element.outerHTML)[0];
           expectedText = expectedDom.body.firstChild.src;
           gotText = this.tuningRoutine(nodeToCssMap, ...coeffs)(sourceDom).map(fnode => fnode.element.src)[0];
+          if(expectedText.includes('?')){
+            expectedText = expectedText.substr(0,expectedText.indexOf('?'));
+          }
+          if(gotText.includes('?')){
+            gotText = gotText.substr(0,gotText.indexOf('?'));
+          }
+
         } else if (this.feature === 'title') {
           //compare innerHTML text of titles
           expectedText = expectedDom.head.firstChild.innerHTML;
@@ -205,9 +298,9 @@ class DiffStats {
         }
 
         // Uncomment for debugging:
-        //console.log(leven(expectedText, gotText), expectedText.length, leven(expectedText, gotText)/expectedText.length);
         // console.log('Got:\n' + gotText);
         // console.log('\nExpected:\n' + expectedText);
+        // console.log(this.deviation, this.numTests, expectedText === gotText);
     }
 
     score() {
@@ -247,7 +340,7 @@ function deviationScore(dataMap, folders, feature, coeffs = []) {
 
     //For each test file, create the object-> css map, and run the comparison function
     folders.forEach(function(store){
-      if(['macys', 'swatch'].includes(store)){return;} // HACK: random jsdom script tag parsing errors, ignore for now
+      // console.log(store);
       stats.compare(dataMap[store][feature], dataMap[store].sourceDom, dataMap[store].nodeToCssMap, coeffs);
       // console.log(stats.score());
     });
@@ -262,7 +355,6 @@ function deviationScore(dataMap, folders, feature, coeffs = []) {
 function createDataMap(folders){
   let dataMap = {};
   folders.forEach(function(store){
-    if(['macys', 'swatch'].includes(store)){return;} // HACK: random jsdom script tag parsing errors, ignore for now
     const domFromFile = fileName => staticDom(readFileSync(join(dirname(__dirname), 'fathom-products', 'product_classification_test_data', store, fileName)));
     const sourceDom = domFromFile('source.html');
     const nodeToCssMap = createDict(store, sourceDom);
@@ -295,14 +387,14 @@ if (require.main === module) {
         /** Nudge a random coefficient in a random direction by 0.5. */
         randomTransition(coeffs) {
             const ret = coeffs.slice();  // Make a copy.
-            ret[Math.floor(Math.random() * coeffs.length)] += Math.floor(Math.random() * 2) ? -.1 : .1;
+            ret[Math.floor(Math.random() * coeffs.length)] += Math.floor(Math.random() * 2) ? -.3 : .3;
             return ret;
         }
     }
 
     //For each feature, calculate score
     Object.keys(tuningRoutines).forEach(function(f){
-      // const annealer = new ProductOptimizer(f);
+      const annealer = new ProductOptimizer(f);
       // tuningRoutines[f].coeffs = annealer.anneal();
       console.log('Tuned coeficients:', tuningRoutines[f].coeffs);
       console.log('% difference from ideal:', deviationScore(dataMap, folders, f, tuningRoutines[f].coeffs));
